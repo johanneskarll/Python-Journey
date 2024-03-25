f10b84a4dd7c83b4cb5181de9eafbd6b67d4cab9
Johannes Karl <karlrepository@gmail.com>
2024-03-25 18:23:15 +0700

    new ipynb

--- 100 Days of Code/38. flask-secrets/main.py
+++ 100 Days of Code/38. flask-secrets/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, InputRequired, Length
 
 class MyForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), InputRequired(), Length(min= 8)])
     submit = SubmitField(label='Log in')
 
 app = Flask(__name__)
@@ -15,9 +15,18 @@ app.secret_key = "some secret string"
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     LoginForm = MyForm()
+    if LoginForm.validate_on_submit():
+        if (LoginForm.email.data == "admin@email.com" and LoginForm.password.data == "12345678"):
+            # @app.route("/success")
+            # def success():
+                return render_template('success.html')
+        else :
+            # @app.route("/denied")
+            # def denied():
+                return render_template('denied.html')
     return render_template('login.html', form=LoginForm)
 
 
